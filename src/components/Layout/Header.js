6b33d0b57c2df5068c7ba163da54280b775ad9fb
dani-lee <ldb1213@gmail.com>
2022-03-21 15:10:21 +0900

    Add CartContext, functions for Cart

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -9,7 +9,7 @@ const Header = (props) => {
     <React.Fragment>
       <header className={classes.header}>
         <h1>FoodOrder</h1>
-        <HaederCartButton />
+        <HaederCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="foods" />
